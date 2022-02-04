29409fd2560dfb972369f6bcdb8706d722537ac0
Jon Michael <jmvlu27@hotmail.com>
2022-02-04 16:07:56 -0500

    add render message

--- js/app.js
+++ js/app.js
@@ -21,9 +21,9 @@ let statusMessage = document.querySelector('#message')
 // 3.1) Call an initialize function
 init() 
 
-	
+// 3.2.1) Initialize the board array to 9 nulls to represent empty squares.
 function init(){
-  boardArr = [1, 1, null, null, -1, null, null, null, 1]
+  boardArr = [1, 1, 1, -1, -1, 1, null, null, 1]
   turn = 1
   winner = null
   render() 
@@ -35,16 +35,35 @@ function render(){
     //console.log(square)
     if(square === 1) {
       squareElems[idx].textContent ="X" 
+      squareElems[idx].style.color ="red" 
+      squareElems[idx].style.background = "blue" 
       
       
     }
     if(square === -1) {
       squareElems[idx].textContent = "O"
+      squareElems[idx].style.color = "blue"
+      squareElems[idx].style.background = "red"
       
     }
-    
+    // 3.3.2) Render a message reflecting the currrent game state:
   })
-  
+
+  //let winner = "5"
+
+  if (winner === null) {
+    statusMessage.textContent = `it's ${turn ===1 ? 'X' : 'O'}'s turn`
+  }
+
+  else if (winner === 'T') {
+    statusMessage.textContent = "It's a tie!"
+  }
+
+  else {
+    statusMessage.textContent = `Congratulations, ${winner === 1 ? 'X' : 'O'}  wins!)`
+  }
+
+
 }
 
 
